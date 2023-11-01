635c7bb8b57f77f96b9037c056d9c3a389e9724c
Katarzyna Waszczuk <katarzynawaszczuk.kontakt@gmail.com>
2023-11-01 18:08:50 +0100

    Creating home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  backgroundImage = 'assets/images/portrait.png';
+
+  getContainerStyles() {
+    return `  
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    `;
+  }
+
+  getBackgroundImageStyles() {
+    return `
+     width: 652px;
+    `;
+  }
+}
