0af78e4fd859179cc9efcb71507ec86c0a88874e
Katarzyna Waszczuk <katarzynawaszczuk.kontakt@gmail.com>
2023-11-03 21:54:41 +0100

    biography component

--- src/app/biography/biography.component.ts
+++ src/app/biography/biography.component.ts
@@ -21,4 +21,15 @@ export class BiographyComponent {
     width: 280px;
     `
   }
+
+  questions: any[] = [
+    {id: 1, text: 'Kiedy i gdzie urodził się Krzysztof?'},
+    {id: 2, text: 'Kiedy zmarł Krzysztof?'},
+    {id: 3, text: 'Co studiował?'},
+    {id: 4, text: 'Jak poznał Basie?'}
+  ];
+
+  answers: any[] = [
+    {text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, magni exercitationem eos iusto iste'}
+  ]
 }
