493335141990095d94c56deada74e573c43cab15
Katarzyna Waszczuk <katarzynawaszczuk.kontakt@gmail.com>
2023-11-05 21:26:57 +0100

    modified BiographyComponent

--- src/app/biography/biography.component.ts
+++ src/app/biography/biography.component.ts
@@ -32,5 +32,29 @@ export class BiographyComponent {
 
   answers: any[] = [
     {text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, magni exercitationem eos iusto iste'}
+  ];
+
+  poems: any[] = [
+    {id: 1, text: `W oczach psów można unieść nieba biały tulipan.
+    Kogo kochasz kochając przydrożne psy,
+    chodząc w ulicach wiatrem spalonych,
+    na szybach zastygając oczami szklanymi jak łzy?
+    Kogo kochasz kochając w ich oczach maleńkie
+    kobiety orientalnych snów,
+    kiedy boisz się twarz ich jak kwiat wziąć do ręki
+    i ostrożnie układasz mozaiki słów?
+    Kogo kochasz? w tych oczach zastygł czas
+    dyluwialnych, zwierzęcych epok
+    i żółte, fosforyczne talerze gwiazd
+    wprawione w oczy jak w niebo.
+    Jakże spokojne ich twarze obrosłe
+    sierścią - osadem - miłością lat,
+    kiedy bijesz na oślep rękami jak wiosłem
+    we wrogi obraz nieba i ziemi, w świat.
+    Rzuć, rzuć magię wirujących kół,
+    podnieś ręce do potęgi łap,
+    wtul się w sierść przyjacielską i płyń
+    przez zarosłe czaszkami wybrzeża lat.`}
   ]
+
 }
