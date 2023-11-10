c9ce02934380a536550b2c44a321b8b9f5616e68
Katarzyna Waszczuk <katarzynawaszczuk.kontakt@gmail.com>
2023-11-10 19:08:31 +0100

    favicon was added

---
+++ src/app/navigation.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NavigationService {
+
+  constructor() { }
+
+  private showNavigation = false;
+
+  setShowNavigation(show: boolean) {
+    this.showNavigation = show;
+  }
+
+  getShowNavigation() {
+    return this.showNavigation;
+  }
+}
