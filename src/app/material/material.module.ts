94c1e5cf43d93e47dbde8cd3eb76eb0abadf0080
Katarzyna Waszczuk <katarzynawaszczuk.kontakt@gmail.com>
2023-12-17 17:29:07 +0100

    Organizing styles

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from "@angular/core";
-
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatExpansionModule} from '@angular/material/expansion';
