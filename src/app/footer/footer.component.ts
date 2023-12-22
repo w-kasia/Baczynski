f7a3cbd013c3170f70091960fae080bc65460009
Katarzyna Waszczuk <katarzynawaszczuk.kontakt@gmail.com>
2023-12-22 22:57:49 +0100

    Content of FooterComponent modified

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  constructor(private router: Router) {}
+
+  goToContact() {
+    this.router.navigate(['/kontakt']);
+  }
 }
