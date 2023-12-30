c974b1efba3ef2589210635eacd7b1b901adb03c
Katarzyna Waszczuk <katarzynawaszczuk.kontakt@gmail.com>
2023-12-30 19:05:24 +0100

    Unit tests added for FooterComponent

--- src/app/footer/footer.component.spec.ts
+++ src/app/footer/footer.component.spec.ts
@@ -1,21 +1,58 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { FooterComponent } from './footer.component';
+import { FooterComponent } from './footer.component';
+import { By } from '@angular/platform-browser';
+import { AppRoutingModule, routingComponents } from '../app-routing.module';
 
-// describe('FooterComponent', () => {
-//   let component: FooterComponent;
-//   let fixture: ComponentFixture<FooterComponent>;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [FooterComponent]
-//     });
-//     fixture = TestBed.createComponent(FooterComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+describe('FooterComponent', () => {
+  let component: FooterComponent;
+  let fixture: ComponentFixture<FooterComponent>;
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [AppRoutingModule],
+      declarations: [FooterComponent, routingComponents]
+    });
+    fixture = TestBed.createComponent(FooterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the footer component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should display the correct email address', () => {
+    const emailLink = fixture.debugElement.query(By.css('.emailLink')).nativeElement;
+
+    expect(emailLink.href).toContain('mailto:kontakt@baczynski.com');
+    expect(emailLink.textContent).toBe('kontakt@baczynski.com');
+  });
+
+  it('should navigate to the contact page when the button is clicked', () => {
+    const button = fixture.debugElement.query(By.css('.contactBtn')).nativeElement;
+    spyOn(component, 'goToContact');
+    button.click();
+
+    expect(component.goToContact).toHaveBeenCalled();
+  });
+
+  it('should have the correct router links', () => {
+    const routerLinks = fixture.debugElement.queryAll(By.css('.footerListItem'));
+
+    const expectedLinks = ['/aktualnosci', '/biografia', '/works', '/galeria'];
+
+    expect(routerLinks.length).toBe(expectedLinks.length);
+
+    routerLinks.forEach((link, index) => {
+      expect(link.nativeElement.getAttribute('routerLink')).toBe(expectedLinks[index]);
+    });
+    });
+
+    it('should display the correct copyright information', () => {
+      const copyrightText = fixture.debugElement.query(By.css('.copyright p')).nativeElement;
+
+      expect(copyrightText.textContent).toContain('© Baczyński 2023');
+    });
+  });
