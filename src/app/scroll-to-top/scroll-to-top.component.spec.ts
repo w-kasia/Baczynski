ba78adff373f0408ff2cb94a98ad8f7df04cf68d
Katarzyna Waszczuk <katarzynawaszczuk.kontakt@gmail.com>
2024-01-03 15:58:50 +0100

    Unit tests for ScrollToTopComponent added

--- src/app/scroll-to-top/scroll-to-top.component.spec.ts
+++ src/app/scroll-to-top/scroll-to-top.component.spec.ts
@@ -1,21 +1,29 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { ScrollToTopComponent } from './scroll-to-top.component';
+import { ScrollToTopComponent } from './scroll-to-top.component';
 
-// describe('ScrollToTopComponent', () => {
-//   let component: ScrollToTopComponent;
-//   let fixture: ComponentFixture<ScrollToTopComponent>;
+describe('ScrollToTopComponent', () => {
+  let component: ScrollToTopComponent;
+  let fixture: ComponentFixture<ScrollToTopComponent>;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ScrollToTopComponent]
-//     });
-//     fixture = TestBed.createComponent(ScrollToTopComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ScrollToTopComponent]
+    });
+    fixture = TestBed.createComponent(ScrollToTopComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create the scroll-to-top component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should scroll to top when scrollToTop is called', () => {
+    spyOn(window, 'scrollTo');
+
+    component.scrollToTop();
+
+    expect(window.scrollTo).toHaveBeenCalled();
+  });
+});
