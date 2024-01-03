30a31bb757c339a06ad5bbaeaf9b3493c37754af
Katarzyna Waszczuk <katarzynawaszczuk.kontakt@gmail.com>
2024-01-03 19:39:52 +0100

    Unit tests for SearchComponent added

--- src/app/search/search.component.spec.ts
+++ src/app/search/search.component.spec.ts
@@ -1,21 +1,32 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { SearchComponent } from './search.component';
+import { SearchComponent } from './search.component';
+import { FormsModule } from '@angular/forms';
 
-// describe('SearchComponent', () => {
-//   let component: SearchComponent;
-//   let fixture: ComponentFixture<SearchComponent>;
+describe('SearchComponent', () => {
+  let component: SearchComponent;
+  let fixture: ComponentFixture<SearchComponent>;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [SearchComponent]
-//     });
-//     fixture = TestBed.createComponent(SearchComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [FormsModule],
+      declarations: [SearchComponent]
+    });
+    fixture = TestBed.createComponent(SearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create the search component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should emit search text when onSearchTextChanged is called', () => {
+    spyOn(component.searchTextChanged, 'emit');
+
+    component.enteredSearchValue = 'test';
+    component.onSearchTextChanged();
+
+    expect(component.searchTextChanged.emit).toHaveBeenCalledWith('test');
+  });
+});
