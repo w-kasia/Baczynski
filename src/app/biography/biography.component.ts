12de95276dc945bf23563ccba3493b57f129ecea
Katarzyna Waszczuk <katarzynawaszczuk.kontakt@gmail.com>
2024-01-06 21:37:31 +0100

    Styles for BiographyComponent modified

--- src/app/biography/biography.component.ts
+++ src/app/biography/biography.component.ts
@@ -1,11 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
-// interface Questions {
-//   question: any;
-//   answer: any;
-// }
-
 interface selectedQuestion {
   answer: any;
 }
@@ -16,26 +11,19 @@ interface selectedQuestion {
 })
 export class BiographyComponent {
 
-  motherImage = 'assets/images/mother.png';
-  butterfly = 'assets/images/butterfly.png';
-  teenage = 'assets/images/teenage.png';
-  quoteIcon = 'assets/quote.svg';
   menuIcon = 'menu';
-
-
-  quote = `"Na imię mi było Krzysztof,
+  quote = `"Na imię mi było Krzysztof, 
   i jeszcze ciało - to tak niewiele".`
 
   questions: any[] = [];
   selectedQuestion: selectedQuestion = {} as selectedQuestion;
-
 
   constructor(private http: HttpClient) {
     this.fetchQuestions();
   }
 
   fetchQuestions() {
-    this.http.get<any[]>('assets/data.json').subscribe(data => {
+    this.http.get<any[]>('assets/faq.json').subscribe(data => {
       this.questions = data;
       this.selectedQuestion = this.questions[0];
     });
@@ -45,9 +33,7 @@ export class BiographyComponent {
     this.selectedQuestion = question;
   }
 
-
-  poems: any[] = [
-    {id: 1, text: `Nas nauczono. Nie ma miłości.
+  poem: string = `Nas nauczono. Nie ma miłości.
     Jakże nam jeszcze uciekać w mrok
     przed żaglem nozdrzy węszących nas,
     przed siecią wzdętą kijów i rąk,
@@ -58,7 +44,6 @@ export class BiographyComponent {
     Wstajemy nocą. Ciemno jest, ślisko.
     Szukamy serca – bierzemy w rękę,
     nasłuchujemy: wygaśnie męka,
-    ale zostanie kamień – tak – głaz.`}
-  ]
+    ale zostanie kamień – tak – głaz.`
 
 }
